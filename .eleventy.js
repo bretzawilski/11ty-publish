54776a3f0f2eeb123dc49610369625c11ea01967
Bret Zawilski <bret.zawilski@gmail.com>
2022-06-12 14:32:23 +0100

    Folder restructure

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = function(eleventyConfig) {
   // Set custom directories for input, output, includes, and data
   return {
     dir: {
-      input: "../serendipity-machine/Public/",
+      input: "./",
       includes: "_includes",
       data: "_data",
       output: "_site"
